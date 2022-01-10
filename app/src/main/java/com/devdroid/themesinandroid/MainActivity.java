e61a067544abdb6838b96abfe54feed63b8c687c
Abhiram Raghunath Deshpande <designing.developer111@gmail.com>
2022-01-10 19:58:50 +0530

    First Commit

---
+++ app/src/main/java/com/devdroid/themesinandroid/MainActivity.java
@@ -0,0 +1,27 @@
+package com.devdroid.themesinandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
